8d301d78617f203e8412c560cd12b6b14c8c276f
Yinfang <suyinfang678@outlook.com>
2026-09-01 19:49:18 +1000

    Changed structure

--- week1/log_audit.sh
+++ week1/log_audit.sh
@@ -2,22 +2,6 @@
 
 set -euo pipefail
 
-folder="${1:-}"
-scanned=0
-flagged=0
-
-if [[ ! -d "$folder" ]]; then
-    exit 3
-fi
-
-for file in "$folder"/*.log; do
-    error_count=$(grep "ERROR" "$file" | wc -l)
-    echo "$error_count errors found in $file"
-
-if [[$error-count > 10]]; then
-    flagged=$((flagged+1))
-fi
-
 log(){
     scanned=$((scanned+1))
     echo "[INFO] $*"
@@ -27,3 +11,27 @@ die(){
     log "[ERROR] $1"
     exit "$2"
 }
+
+scanned=0
+flagged=0
+
+scanned=$((scanned+1))
+
+if [[ "$#" -ne 1 ]]; then
+    die "Usage: $0 <folder>" 1
+fi
+
+folder="$1"
+
+if [[ ! -d "$folder" || ! -r "$folder" ]]; then
+    exit 3
+fi
+
+for file in "$folder"/*.log; do
+    error_count=$(grep "ERROR" "$file" | wc -l)
+    echo "$error_count errors found in $file"
+
+if [[ $error_count -gt 10 ]]; then
+    flagged=$((flagged+1))
+fi
+
